39b7e9088692660e9201833919d730ec1370e952
sashase <semenchuksasha21@gmail.com>
2023-10-20 14:41:25 +0300

    Width Extended & Footer Updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,40 @@
+import Link from "next/link"
+
 export default function Footer() {
   return (
-    <footer className="text-azurish mx-auto">© 2023 Oleksandr Semenchuk</footer>
+    <footer className="text-beau flex items-end justify-between w-100 md:w-4/5 mx-0 md:mx-auto mt-5 md:mt-0 animate-float-in-delay-1 opacity-0">
+      <Link
+        href="https://github.com/sashase/portfolio"
+        className="hover:text-primary transition-all">
+        © 2023 Oleksandr Semenchuk
+      </Link>
+      <div className="flex flex-col gap-1">
+        <span className="font-bold text-primary text-lg self-end">
+          Contacts
+        </span>
+        <div className="flex flex-col md:flex-row gap-1 md:gap-5">
+          <Link
+            href="https://github.com/sashase"
+            className="hover:text-primary transition-all ">
+            GitHub
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/oleksandr-semenchuk-58a901240/"
+            className="hover:text-primary transition-all">
+            LinkedIn
+          </Link>
+          <Link
+            href="https://cv.djinni.co/2b/0b80d95959084a04e25a417ca5a56d/Oleksandr_Semenchuk.pdf"
+            className="hover:text-primary transition-all">
+            CV
+          </Link>
+          <Link
+            href="mailto:contactme@semenchuk.cc"
+            className="hover:text-primary transition-all">
+            E-Mail
+          </Link>
+        </div>
+      </div>
+    </footer>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next"
-import Image from "next/image"
 import Link from "next/link"
 
 export const metadata: Metadata = {
@@ -22,33 +21,6 @@ export default function Home() {
             crafting digital solutions. I bring creative and functional concepts
             to life. <br /> Let's build something amazing together!
           </p>
-        </div>
-        <div className="hidden md:flex flex-col gap-2 absolute top-5 -start-8">
-          <Link href="https://github.com/sashase">
-            <Image
-              src="/contacts/github.svg"
-              width={20}
-              height={20}
-              alt="github"
-            />
-          </Link>
-
-          <Link href="https://www.linkedin.com/in/oleksandr-semenchuk-58a901240/">
-            <Image
-              src="/contacts/linkedin.svg"
-              width={20}
-              height={20}
-              alt="linkedin"
-            />
-          </Link>
-
-          <Link href="https://cv.djinni.co/2b/0b80d95959084a04e25a417ca5a56d/Oleksandr_Semenchuk.pdf">
-            <Image src="/contacts/cv.svg" width={20} height={20} alt="cv" />
-          </Link>
-
-          <Link href="mailto:o@semenchuk.cc">
-            <Image src="/contacts/mail.svg" width={20} height={20} alt="mail" />
-          </Link>
         </div>
       </div>
       <div className="flex row-span-2">
